3d1acfd213245752759560209bcb0049c59869b5
TONYSUKANDAR <SUKANDAR@MAIL.com>
2017-09-05 12:11:44 +0200

    One-to-Many: Models

---
+++ db/migrate/20170905094002_create_rooms.rb
@@ -0,0 +1,23 @@
+class CreateRooms < ActiveRecord::Migration[5.1]
+  def change
+    create_table :rooms do |t|
+      t.string :home_type
+      t.string :room_type
+      t.integer :accommodate
+      t.integer :bedroom_count
+      t.integer :bathroom_count
+      t.string :listing_name
+      t.text :description
+      t.string :address
+      t.boolean :has_tv
+      t.boolean :has_kitchen
+      t.boolean :has_airco
+      t.boolean :has_heating
+      t.boolean :has_internet
+      t.decimal :price
+      t.boolean :active
+
+      t.timestamps
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,32 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170904103714) do
+ActiveRecord::Schema.define(version: 20170905100130) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "rooms", force: :cascade do |t|
+    t.string "home_type"
+    t.string "room_type"
+    t.integer "accommodate"
+    t.integer "bedroom_count"
+    t.integer "bathroom_count"
+    t.string "listing_name"
+    t.text "description"
+    t.string "address"
+    t.boolean "has_tv"
+    t.boolean "has_kitchen"
+    t.boolean "has_airco"
+    t.boolean "has_heating"
+    t.boolean "has_internet"
+    t.decimal "price"
+    t.boolean "active"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.bigint "user_id"
+    t.index ["user_id"], name: "index_rooms_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -32,4 +54,5 @@ ActiveRecord::Schema.define(version: 20170904103714) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "rooms", "users"
 end
